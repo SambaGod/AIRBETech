a50dc04b859585363cf0edd4a6a6b897ba2b27ff
Daniel Gruber <daniel@ibpg.eu>
2021-07-02 08:29:49 +0200

    - change sidebar

--- Webservice/src/components/partials/AppSidebar.vue
+++ Webservice/src/components/partials/AppSidebar.vue
@@ -19,27 +19,27 @@
           </a>
         </li>
         <li class="nav-item nav-category">Main Menu</li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/"><span class="menu-title">My Tasks</span></router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/tasks/"><span class="menu-title">Create Task</span></router-link>
-        </li>
-        <li class="nav-item">
+         <li class="nav-item">
           <a class="nav-link" v-b-toggle="'ui-basic'">
             <i class="menu-icon typcn typcn-coffee"></i>
-            <span class="menu-title">Patient Data</span>
+            <span class="menu-title">Task Management</span>
             <i class="menu-arrow"></i>
           </a>
           <b-collapse id="ui-basic">
             <ul class="nav flex-column sub-menu">
               <li class="nav-item">
-                <router-link class="nav-link" to="/patient/"
-                  >Main Patient Data</router-link
-                >
+                <router-link class="nav-link" to="/"><span class="menu-title">My Tasks</span></router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/tasks/"><span class="menu-title">Create Task</span></router-link>
               </li>
             </ul>
           </b-collapse>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/patient/"
+            ><span class="menu-title">Patient Data</span></router-link
+          >
         </li>
         <li class="nav-item">
           <router-link class="nav-link" to="/messages/"><span class="menu-title">Messages</span></router-link>
